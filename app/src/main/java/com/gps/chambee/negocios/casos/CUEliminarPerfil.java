a9c03df38c39f15cc27f813c69abbdb9774dd24f
Mata-Alejandro <mata.avila.16200@itsmante.edu.mx>
2019-10-21 12:29:32 -0500

    presentadores y casos de uso de calle,colonia,perfil,etc.

---
+++ app/src/main/java/com/gps/chambee/negocios/casos/CUEliminarPerfil.java
@@ -0,0 +1,30 @@
+package com.gps.chambee.negocios.casos;
+
+import android.content.Context;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.gps.chambee.servicios.web.ServicioWeb;
+import com.gps.chambee.servicios.web.implementaciones.SWEliminarPerfil;
+
+public class CUEliminarPerfil extends CasoUso<String> {
+
+    public CUEliminarPerfil(Context context, EventoPeticionAceptada<String> eventoPeticionAceptada, EventoPeticionRechazada eventoPeticionRechazada) {
+        super(context, eventoPeticionAceptada, eventoPeticionRechazada);
+    }
+
+    @Override
+    protected ServicioWeb definirServicioWeb() {
+        return new SWEliminarPerfil(context, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                eventoPeticionAceptada.alAceptarPeticion(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                eventoPeticionRechazada.alRechazarOperacion();
+            }
+        });
+    }
+}
