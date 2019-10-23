d52611b277b363059ccffb47925a1a5e4d480a5a
Jaquez <maryfer.jaquezkimlee@gmail.com>
2019-10-23 14:36:31 -0500

    Agregadas actividades para layouts

--- app/src/main/java/com/gps/chambee/ui/actividades/RecuperarPasswordActivity.java
+++ app/src/main/java/com/gps/chambee/ui/actividades/RecuperarPasswordActivity.java
@@ -3,18 +3,14 @@ package com.gps.chambee.ui.actividades;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
 import com.gps.chambee.R;
 
-public class MainActivity extends AppCompatActivity {
-
-    private Button bRegistrar;
+public class RecuperarPasswordActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.recuperar_password_activity);
     }
 }
