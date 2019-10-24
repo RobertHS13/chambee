0d1e20ab6e247243bc68bbfd31ae1d89e4799df0
RobertHS13 <robertohs13000@gmail.com>
2019-10-24 12:09:28 -0500

    Se agregaron nuevos CU

---
+++ app/src/main/java/com/gps/chambee/negocios/casos/CUActualizarOficio.java
@@ -0,0 +1,30 @@
+package com.gps.chambee.negocios.casos;
+
+import android.content.Context;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.gps.chambee.servicios.web.ServicioWeb;
+import com.gps.chambee.servicios.web.implementaciones.SWActualizarOficio;
+
+public class CUActualizarOficio extends CasoUso<String> {
+
+    public CUActualizarOficio(Context context, EventoPeticionAceptada<String> eventoPeticionAceptada, EventoPeticionRechazada eventoPeticionRechazada) {
+        super(context, eventoPeticionAceptada, eventoPeticionRechazada);
+    }
+
+    @Override
+    protected ServicioWeb definirServicioWeb() {
+        return new SWActualizarOficio(context, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                eventoPeticionAceptada.alAceptarPeticion(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                eventoPeticionRechazada.alRechazarOperacion();
+            }
+        });
+    }
+}
